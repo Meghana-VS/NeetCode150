95e38cf0330bd9820843de8dd818ccdd6e10c042
Meghana-VS <meghavs7022@gmail.com>
2025-10-22 21:04:43 +0530

    Group Anagrams

--- src/arrays_and_hashing/GroupAnagrams.java
+++ src/arrays_and_hashing/GroupAnagrams.java
@@ -9,11 +9,27 @@ public class GroupAnagrams {
         System.out.println(Arrays.toString(res.toArray()));
     }
     public static List<List<String>> groupAnagrams(String[] strs) {
+//        Map<String, List<String>> map = new HashMap<>();
+//        for(String s : strs){
+//            char[] ch = s.toCharArray();
+//            Arrays.sort(ch);
+//            String key = new String(ch);
+//            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+//        }
+//        return new ArrayList<>(map.values());
+
+//***************************************************************************
         Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
-            char[] ch = s.toCharArray();
-            Arrays.sort(ch);
-            String key = new String(ch);
+            int[] count = new int[26];
+            for(char ch : s.toCharArray()){
+                count[ch - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int num : count){
+                sb.append(num).append("#");
+            }
+            String key = sb.toString();
             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(map.values());
