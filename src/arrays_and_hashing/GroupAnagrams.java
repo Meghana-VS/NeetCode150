43b3a3c95349c2cceb55d240e0921b7676afb69e
Meghana-VS <meghavs7022@gmail.com>
2025-10-22 20:45:15 +0530

    Group Anagrams

---
+++ src/arrays_and_hashing/GroupAnagrams.java
@@ -0,0 +1,21 @@
+package arrays_and_hashing;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"act","pots","tops","cat","stop","hat"};
+        List<List<String>> res = groupAnagrams(strs);
+        System.out.println(Arrays.toString(res.toArray()));
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
