e7ae7f4bc744665cc674cf5beab37c2bcd9a9945
Meghana-VS <meghavs7022@gmail.com>
2025-10-30 20:45:25 +0530

    Longest Consecutive Sequence

--- src/arrays_and_hashing/LongestConsecutiveSequence.java
+++ src/arrays_and_hashing/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package arrays_and_hashing;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
@@ -8,23 +10,44 @@ public class LongestConsecutiveSequence {
         System.out.println(longestConsecutive(nums));
     }
     public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
         int longestSequence = 1;
-        int count = 0;
-        int lastSmallest = Integer.MIN_VALUE;
 
-        Arrays.sort(nums);
-        for (int num : nums) {
-            if (num - 1 == lastSmallest) {
-                count++;
-                lastSmallest = num;
-            } else if (num != lastSmallest) {
-                count = 1;
-                lastSmallest = num;
+        for (int num : nums) set.add(num);
+
+        for (int element : set) {
+            // if 'element' is a starting number
+            if (!set.contains(element - 1)) {
+                // find consecutive numbers
+                int count = 1;
+                int x = element;
+                while(set.contains(x+1)){
+                    count++;
+                    x = x + 1;
+                }
+                longestSequence = Math.max(longestSequence, count);
             }
-            longestSequence = Math.max(longestSequence, count);
         }
         return longestSequence;
     }
+//****************************************************************************************************
+//        int longestSequence = 1;
+//        int count = 0;
+//        int lastSmallest = Integer.MIN_VALUE;
+//
+//        Arrays.sort(nums);
+//        for (int num : nums) {
+//            if (num - 1 == lastSmallest) {
+//                count++;
+//                lastSmallest = num;
+//            } else if (num != lastSmallest) {              // if 'num' is a starting number
+//                count = 1;
+//                lastSmallest = num;
+//            }
+//            longestSequence = Math.max(longestSequence, count);
+//        }
+//        return longestSequence;
+//    }
 
 //**************************************************************************************************************
 //        int longestSequence = Integer.MIN_VALUE;
